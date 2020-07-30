a491f8f8e386d283c1c1a08197365fa56317278c
Ronertas <63651619+Ronertas@users.noreply.github.com>
2020-07-30 23:53:26 +0300

    added comment

--- Java/Hello/HelloWorld.java
+++ Java/Hello/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public void runHello () {
 		FileResource res = new FileResource("hello_unicode.txt");
 		for (String line : res.lines()) {
-			System.out.println(line);
+			System.out.println(line); /* prints all languages */
 		}
 	}
 }
